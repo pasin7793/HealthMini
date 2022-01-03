0f303852a79a8656f156bb70eca071936b85bd12
pasin7793 <pasin7793@naver.com>
2022-01-03 11:50:10 +0900

    Initial Commit

---
+++ HealthMini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthMini
+//
+//  Created by 임준화 on 2022/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
